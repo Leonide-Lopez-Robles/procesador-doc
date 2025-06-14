fa0720667aa2fdf3c153b1c75a730a16363699da
Leonide Lopez Robles <lorl030615@gs.utm.mx>
2025-06-14 17:30:27 -0600

    se hizo un pequeño cambio en page tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-white to-gray-200 flex flex-col items-center pt-20 text-center">
+    <div className="min-h-screen bg-gradient-to-b from-white to-gray-200 flex flex-col items-center pt-5 text-center">
 
       <h1 className="text-4xl font-bold mb-2">Procesador de Documentos</h1>
       <p className="text-gray-700 mb-8">Extrae, traduce y resume texto de imágenes y PDFs</p>
